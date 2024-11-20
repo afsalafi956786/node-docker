7790f8159debb03ff90327c0cc91820447412030
Afsal <afsal.xyug@gmail.com>
2024-11-20 21:16:08 +0530

    changes done

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -5,6 +5,7 @@ import { getAllUser,UserSignup,userLogin } from '../controller/userController.js
 const router=express.Router();
 
 router.get('/',getAllUser);
+//ser
 router.post('/signup',UserSignup);
 router.post('/login',userLogin);
 
